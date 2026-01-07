3604664a4bd3bdd5cc80b5bc83ae36522ad6682a
muthusivam <muthusivam.edu@gmail.com>
2026-01-07 21:34:11 +0200

    added some functions according to the teacher

--- Main.py
+++ Main.py
@@ -5,12 +5,15 @@ import glob
 #Function to create the tree edges 
 def tree_edges(prec,V):
     tree=[]
+    new_tree=[]
     for i in range(1,V+1):
-        if prec[i] !=i:
+        if prec[i] !=i and prec[i]!=0 :
             lst=[prec[i],i]
             lst.sort()
             tree.append(lst)
             tree.sort()
+         
+   
     return tree
 
 
@@ -22,14 +25,15 @@ def text_inp():
     for line in file:
         a,b=line.strip().split(' ')
         if a not in vertices:
-            vertices.append(a)
+            vertices.append(int(a))
         if b not in vertices:
-            vertices.append(b)
+            vertices.append(int(b))
         lst=[int(a),int(b)]
         lst.sort()
         edges.append(lst)
+    vertices=max(vertices)
     file.close()
-    return edges,len(vertices)
+    return edges,vertices
 
 #Function to create the adjacency matrix
 def adjacet_matrix(edges,V):
@@ -40,24 +44,59 @@ def adjacet_matrix(edges,V):
     return adj
 
 # DFS algorithm Function 
-def dfs(adj,start,V):
-    prec=[0]*(V+1)
+def dfs(adj,start,V,v_count,prec):
+    #prec=[0]*(V+1)
     prec[start]=start
     curr=start
+    v_count+=1
     while True:
         neigbours=adj[curr]
         for val in neigbours:
             if prec[val]==0:
                 prec[val]=curr
                 curr=val
+                v_count +=1
                 break
         else:   
             if curr==start:
                 break
             else:
                 curr=prec[curr]
-    return prec
+    return prec,v_count
 
+def first(prec):
+    for i in range(1,len(prec)):
+       if prec[i]==0:
+        return i
+
+
+def connect_comp_tree(V,adj):
+    count=1
+    vis=[]
+    start=1
+    prec=[0]*(V+1)
+    ls=[]
+    while count < V:
+        for i in range(V):
+            if prec[i]!=0 and i not in vis:
+                vis.append(i)
+        for i in range(1,len(prec)):
+            if prec[i] == 0 and i not in vis:
+                prec = [0]*(V+1)
+                start = i
+                prec,count=dfs(adj, start, V, count, prec)
+                tr=tree_edges(prec, V)
+                if adj[i]==[]:
+                    ls.append([[i,i]])
+                else:
+                    ls.append(tr)
+                break
+            
+                
+
+    for i in range(len(ls)):
+        print(f"tree in Connected Component-{i+1}: {ls[i]}")
+        dp.plot_graph(ls[i],f"tree-{i+1}")
 
 #Main Method
 if __name__ == "__main__":
@@ -77,17 +116,21 @@ if __name__ == "__main__":
         print(f"{i}:{adj[i]}")
 
 
-    start=1
-    prec=dfs(adj,start,V)
-    print(f"Prec:{prec[1:]}")
-
-    if 0 in prec[1:]:
-        print("Graph is disconnected")
-    else:
-        tree=tree_edges(prec,V)
-        print("The graph is connected.")
-        print(f"Tree Edges from vertex - {start}: \n {tree}")
-        dp.plot_graph(tree, f" Tree from vertex ({start})")
-    
-
-   
+    ls = []
+    count = 1
+    prec = [0]*(V+1)
+    while count<V:
+        start=first(prec)
+        prec,count=dfs(adj,start,V,count,prec) 
+        print(prec,count)
+        tree = []
+        for i in range(1,len(prec)):
+            for val in edges:
+                if prec[i]==val[0]or prec[i]==val[1]:
+                    tree.append(val)
+                    edges.remove(val)
+        
+        ls.append(tree)
+    for i in range(len(ls)):
+        adj_tre=adjacet_matrix(ls[i],V)
+        print(f"Tree-{i+1}={adj_tre}")
